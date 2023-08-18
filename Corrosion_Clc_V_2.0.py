cef60f2afee301c4f0fc430d66db74c5f3e155a0
Иванов Иван <ivanov.im@nizta.local>
2023-08-18 15:11:25 +0300

    Добавил про внутреннюю среду

--- Corrosion_Clc_V_2.0.py
+++ Corrosion_Clc_V_2.0.py
@@ -63,34 +63,31 @@ outside_the_lining = {
 
 outside_keys_list = list(outside_the_lining.keys())
 
+#Внешняя среда
 
-def proc1(event):
-    selected_value = outside_combox.get()
-    cond = int(float(cond_ent.get()))
-    if selected_value == 'Почва (ρ=200…1000 Ом*см)':
-        m = 0.13 + ((0.06 - 0.13) / (200 - 1000)) * (cond - 1000)
-        outside_the_lining[selected_value] = m
-        result_label_outs.config(text=f'Внешняя среда: {selected_value} | {outside_the_lining[selected_value]} мм/год')
+def proc_out(event):
+    selected_value_outs = outside_combox.get()
+    cond_outs = int(float(cond_outs_ent.get()))
+    if selected_value_outs == 'Почва (ρ=200…1000 Ом*см)':
+        m = 0.13 + ((0.06 - 0.13) / (200 - 1000)) * (cond_outs - 1000)
+        outside_the_lining[selected_value_outs] = m
+        result_label_outs.config(text=f'Внешняя среда: {selected_value_outs} | {outside_the_lining[selected_value_outs]} мм/год')
     else:
-        n = 0.13 + ((0.06 - 0.03) / (1000 - 2000)) * (cond - 2000)
-        outside_the_lining[selected_value] = n
-        result_label_outs.config(text=f'Внешняя среда: {selected_value} | {outside_the_lining[selected_value]} мм/год')
+        n = 0.13 + ((0.06 - 0.03) / (1000 - 2000)) * (cond_outs - 2000)
+        outside_the_lining[selected_value_outs] = n
+        result_label_outs.config(text=f'Внешняя среда: {selected_value_outs} | {outside_the_lining[selected_value_outs]} мм/год')
 
 def on_outcomb_selected(event):
-    selected_value = outside_combox.get()
-    if selected_value == 'Почва (ρ=200…1000 Ом*см)':
-        cond_ent.config(state='normal')
-        cond_ent.focus()
-        cond_ent.delete(0, tk.END)
-    elif selected_value == 'Почва (ρ=1000…2000 Ом*см)':
-        cond_ent.config(state='normal')
-        cond_ent.focus()
-        cond_ent.delete(0, tk.END)
-    result_label_outs.config(text=f'Внешняя среда: {selected_value} | {outside_the_lining[selected_value]} мм/год')
-    #if selected_value in outside_the_lining:
-       # outside_the_lining[selected_value] = value
-        #print(value)
-   # else: print(3)
+    selected_value_outs = outside_combox.get()
+    if selected_value_outs == 'Почва (ρ=200…1000 Ом*см)':
+        cond_outs_ent.config(state='normal')
+        cond_outs_ent.focus()
+        cond_outs_ent.delete(0, tk.END)
+    elif selected_value_outs == 'Почва (ρ=1000…2000 Ом*см)':
+        cond_outs_ent.config(state='normal')
+        cond_outs_ent.focus()
+        cond_outs_ent.delete(0, tk.END)
+    result_label_outs.config(text=f'Внешняя среда: {selected_value_outs} | {outside_the_lining[selected_value_outs]} мм/год')
 
 #Интерфейс выпадающего списка внешней среды
 outside_combox_lbl = tk.Label(ini_frame, text='Определите внешнюю среду:')
@@ -99,16 +96,70 @@ outside_combox=ttk.Combobox(ini_frame, values=outside_keys_list, width=30)
 outside_combox.grid(row = 1, column = 1, sticky='w')
 result_label_outs = tk.Label(post_frame, text='')
 result_label_outs.grid(row = 1, column = 0, sticky='w')
-cond_lbl = tk.Label(ini_frame, text='Введите значение \n электропроводности:')
-cond_lbl.grid(row = 2, column = 0, sticky='w')
-cond_ent = tk.Entry(ini_frame, width=10, bg="white", state='disabled')
-cond_ent.grid(row = 2, column = 1, sticky='w')
-cond_ent.bind('<Return>', proc1)
-cond_ent.bind('<FocusOut>', proc1)
-
+cond_outs_lbl = tk.Label(ini_frame, text='Введите значение \n электропроводности:')
+cond_outs_lbl.grid(row = 2, column = 0, sticky='w')
+cond_outs_ent = tk.Entry(ini_frame, width=10, bg="white", state='disabled')
+cond_outs_ent.grid(row = 2, column = 1, sticky='w')
+cond_outs_ent.bind('<Return>', proc_out)
+cond_outs_ent.bind('<FocusOut>', proc_out)
 outside_combox.bind('<<ComboboxSelected>>', on_outcomb_selected)
 
 
+# Внутренняя
+
+#Перечень исходных параметров из таблиц для расчёта
+inside_the_lining = {
+        'Атмосфера чистая': 0.025,
+        'Атмосфера загрязненная (городская)': 0.125,
+        'Атмосфера, обогащенная SO2': 0.79,
+        'Вода морская, спокойная': 0.125,
+        'Вода морская (скорость течения 6-10 м/с)': 1.25,
+        'Почва (ρ=200…1000 Ом*см)': 0.06,
+        'Почва (ρ=1000…2000 Ом*см)': 0.03
+    }
+
+inside_keys_list = list(inside_the_lining.keys())
+
+def proc_ins(event):
+    selected_value_ins = inside_combox.get()
+    cond_ins = int(float(cond_ins_ent.get()))
+    if selected_value_ins == 'Почва (ρ=200…1000 Ом*см)':
+        x = 0.13 + ((0.06 - 0.13) / (200 - 1000)) * (cond_ins - 1000)
+        inside_the_lining[selected_value_ins] = x
+        result_label_ins.config(text=f'Внутренняя среда: {selected_value_ins} | {inside_the_lining[selected_value_ins]} мм/год')
+    else:
+        y = 0.13 + ((0.06 - 0.03) / (1000 - 2000)) * (cond_ins - 2000)
+        inside_the_lining[selected_value_ins] = y
+        result_label_ins.config(text=f'Внутренняя среда: {selected_value_ins} | {inside_the_lining[selected_value_ins]} мм/год')
+
+
+def on_inscomb_selected(event):
+    selected_value_ins = inside_combox.get()
+    if selected_value_ins == 'Почва (ρ=200…1000 Ом*см)':
+        cond_ins_ent.config(state='normal')
+        cond_ins_ent.focus()
+        cond_ins_ent.delete(0, tk.END)
+    elif selected_value_ins == 'Почва (ρ=1000…2000 Ом*см)':
+        cond_ins_ent.config(state='normal')
+        cond_ins_ent.focus()
+        cond_ins_ent.delete(0, tk.END)
+    result_label_ins.config(text=f'Внутренняя среда: {selected_value_ins} | {outside_the_lining[selected_value_ins]} мм/год')
+
+
+# Интерфейс выпадающего списка внешней среды
+inside_combox_lbl = tk.Label(ini_frame, text='Определите внутреннюю среду:')
+inside_combox_lbl.grid(row=3, column=0, sticky='w')
+inside_combox = ttk.Combobox(ini_frame, values=outside_keys_list, width=30)
+inside_combox.grid(row=3, column=1, sticky='w')
+result_label_ins = tk.Label(post_frame, text='')
+result_label_ins.grid(row=3, column=0, sticky='w')
+cond_ins_lbl = tk.Label(ini_frame, text='Введите значение \n электропроводности:')
+cond_ins_lbl.grid(row=4, column=0, sticky='w')
+cond_ins_ent = tk.Entry(ini_frame, width=10, bg="white", state='disabled')
+cond_ins_ent.grid(row=4, column=1, sticky='w')
+cond_ins_ent.bind('<Return>', proc_ins)
+cond_ins_ent.bind('<FocusOut>', proc_ins)
+inside_combox.bind('<<ComboboxSelected>>', on_inscomb_selected)
 
 
 
